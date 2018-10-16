1d794fa82d6908a0372cae8129fb0d884568df76
Bola Awoyemi <bolaawoyemi@Bolas-MacBook-Air.local>
2018-10-15 23:51:53 -0400

    FIrst Real Commit
    The ID number and Last Name

---
+++ test.asm
@@ -0,0 +1,45 @@
+.text
+	main:
+		li $v0, 11
+		la $a0, 64	# @ (64)
+		syscall
+		sub $a0, $a0, 16 # 0 (48)
+		syscall
+		addi $a0, $a0, 2 # 2 (50)
+		syscall
+		addi $a0, $a0, 6 # 8 (58)
+		syscall 
+		sub $a0, $a0, 2 # 6 (56)
+		syscall
+		addi $a0, $a0, 0 # 6 (56)
+		syscall
+		sub $a0, $a0, 8 # 0 (48)
+		syscall
+		addi $a0, $a0, 10 # 8 (58)
+		syscall
+		sub $a0, $a0, 8 # 2 (50)
+		syscall
+		li $v0, 10
+		syscall
+		\end
+		
+		li $v0, 11
+		la $a0, 65	# A
+		syscall
+		addi $a0, $a0, 54 # w (119)
+		syscall
+		sub $a0, $a0, 8 # o (111)
+		syscall
+		addi $a0, $a0, 10 # y (121)
+		syscall
+		sub $a0, $a0, 20 # e (101)
+		syscall
+		addi $a0, $a0, 8 # m (109)
+		syscall
+		sub $a0, $a0, 4 # i (105)
+		syscall
+		sub $a0, $a0, 61 # , (44)
+		syscall
+		li $v0, 10
+		syscall
+		

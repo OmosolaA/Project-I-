14dd7d993d535e2dafe97132d2a400c9d4044c9f
Bola Awoyemi <bolaawoyemi@Bolas-MacBook-Air.local>
2018-10-19 21:45:17 -0400

    Commit #2
    The complete first line

--- test.asm
+++ test.asm
@@ -1,3 +1,4 @@
+
 .text	
 	main:
 		li $v0, 11
@@ -10,4 +11,15 @@
 		addi $a0, $a0, 6 # 8 (58)
 		syscall 
 		sub $a0, $a0, 2 # 6 (56)
-		syscall	
+		syscall
+		addi $a0, $a0, 0 # 6 (56)
+		syscall
+		sub $a0, $a0, 8 # 0 (48)
+		syscall
+		addi $a0, $a0, 10 # 8 (58)
+		syscall 	
+		sub $a0, $a0, 8 # 2 (50)
+		syscall
+		li $v0, 10
+		syscall
+		\end

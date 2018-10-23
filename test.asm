6752ed150c3a2f3e36574942884c6721bf44c725
Bola Awoyemi <bolaawoyemi@Bolas-MacBook-Air.local>
2018-10-23 19:51:29 -0400

    commit ??

--- test.asm
+++ test.asm
@@ -14,18 +14,17 @@
 		syscall
 		addi $a0, $a0, 0 # 6 (56)
 		syscall
-		sub $a0, $a0, 8 # 0 (48)
+		sub $a0, $a0, 6 # 0 (48)
 		syscall
-		addi $a0, $a0, 10 # 8 (58)
+		addi $a0, $a0, 8 # 8 (58)
 		syscall 	
-		sub $a0, $a0, 8 # 2 (50)
+		sub $a0, $a0, 6 # 2 (50)
 		syscall
-		li $v0, 10
-		syscall
-		\end
 		
 `		li $v0, 11
-		la $a0, 65 # A
+		la $a0, 10 #return 
+		syscall
+		la $a0, 55 # A (65)
 		syscall
 		addi $a0, $a0, 54 # w (119)
 		syscall
@@ -45,4 +44,15 @@
 		syscall
 		addi $a0, $a0, 30 # m (109)
 		syscall 
-		 
+		addi $a0, $a0, 2 # o (111)
+		syscall 
+		addi $a0, $a0, 4 # s (115)
+		syscall
+		sub $a0, $a0, 4 # o (111)
+		syscall 
+		sub $a0, $a0, 3 # l (108)
+		syscall
+		sub $a0, $a0, 11 # a (97)
+		syscall
+		li $v0, 10
+		syscall 		 
